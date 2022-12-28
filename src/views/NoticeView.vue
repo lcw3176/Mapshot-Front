f2fd3e54bfea00cc580f17b7b70dd1048d7f1d1f
lcw3176 <lcw3176@naver.com>
2022-12-28 17:47:00 +0900

    feat: 공지사항 뷰 추가

---
+++ src/views/NoticeView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container mt-5">
+        <NoticeListPage/>
+        <NoticeListPage/>
+        <NoticeListPage/>
+        <NoticeListPage/>
+        <NoticeListPage/>
+
+        <Pagination/>
+    </div>
+   
+</template>
+
+<script>
+import NoticeSummary from '@/components/notice/NoticeSummary.vue';
+import Pagination from '@/components/notice/Pagination.vue';
+
+export default {
+    name: 'NoticeView',
+    components: {
+        NoticeSummary,
+        Pagination,
+    }
+}
+</script>
+
+
+<style scoped>
+@import "@/assets/css/bulma-list.css";
+
+</style>
