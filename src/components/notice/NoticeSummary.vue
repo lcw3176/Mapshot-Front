96125ef18a7c57766c2614f4a22da95a30803a2c
lcw3176 <lcw3176@naver.com>
2022-12-28 17:46:15 +0900

    feat: 공지사항 관련 컴포넌트 업데이트

---
+++ src/components/notice/NoticeSummary.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div class="container mt-5">
+        <div class="list">
+            <div class="list-item box">
+                <div class="list-item-image">
+                    <span class="tag is-info">업데이트</span>
+                </div>
+
+                <div class="list-item-content">
+                    <a href="/notice(post=${post.id})">
+                        <div class="list-item-title">사이트 개편 안내</div>
+                    </a>
+                    <div class="list-item-description">23.01.01
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+
+<script>
+
+export default {
+    name: 'NoticeSummary',
+}
+</script>
