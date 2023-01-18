6355364f3e408174ca3c6426d10987909786e3b6
lcw3176 <lcw3176@naver.com>
2023-01-18 10:28:02 +0900

    feat(notice.js): url 실 서비스로 변경

--- src/store/notice.js
+++ src/store/notice.js
@@ -4,13 +4,13 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/ko'
 
 async function getContent(id) {
-  const response = await axios.get('https://port-0-mapshot-api-ngsnp25lbunrwi4.gksl2.cloudtype.app/api/notice/detail/' + id);
+  const response = await axios.get('https://api.kmapshot.com/notice/detail/' + id);
   return response.data;
 }
 
 
 async function getSummary(id){
-    const response = await axios.get('https://port-0-mapshot-api-ngsnp25lbunrwi4.gksl2.cloudtype.app/api/notice/summary/' + id);
+    const response = await axios.get('https://api.kmapshot.com/notice/summary/' + id);
     return response.data;
 }
 
