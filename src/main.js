51e49bd232ee3706b2302516bb07a62e48751829
lcw3176 <lcw3176@naver.com>
2023-04-18 00:20:11 +0900

    feat: sentry 도입

--- src/main.js
+++ src/main.js
@@ -3,5 +3,19 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from "pinia";
 import VueMobileDetection from 'vue-mobile-detection'
+import * as Sentry from "@sentry/vue";
+
+Sentry.init({
+    App,
+    dsn: "https://1c1f8712a989433cb34416dd0bfde82e@o4505030048284672.ingest.sentry.io/4505030053330944",
+    integrations: [
+        new Sentry.BrowserTracing({
+            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+            tracePropagationTargets: ["kmapshot.com", /^\//],
+        }),
+    ],
+
+    tracesSampleRate: 0.5,
+});
 
 createApp(App).use(createPinia()).use(router).use(VueMobileDetection).mount('#app')
