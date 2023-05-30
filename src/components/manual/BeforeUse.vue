87b1efe2a3bfc5d2d4195d8e080aac025c30668e
lcw3176 <lcw3176@naver.com>
2023-05-30 16:14:24 +0900

    feat: 데스크탑 권장 문구 추가

--- src/components/manual/BeforeUse.vue
+++ src/components/manual/BeforeUse.vue
@@ -1,6 +1,14 @@
 <template>
     <v-card elevation="0" class="ml-10">
         <v-card-title class="text-h5">사용 전</v-card-title>
+
+        <v-card-subtitle>환경</v-card-subtitle>
+        <v-card-text>
+            본 사이트는 데스크탑 환경에 맞게 이루어져 있습니다.
+            원활한 서비스 이용을 위해서는 모바일 사용을 지양해 주세요.
+        </v-card-text>
+
+        <v-card-subtitle>브라우저</v-card-subtitle>
         <v-card-text>
             현재 지원중인 브라우저 목록입니다.
             만약 서비스 이용에 어려움이 있다면, 호환 브라우저 목록을 확인해 주세요.
@@ -14,14 +22,20 @@
                             이름
                         </th>
                         <th class="text-left">
-                            호환여부
+                            지원 여부
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="item in browser" :key="item.name">
                         <td>{{ item.name }}</td>
-                        <td>{{ item.status }}</td>
+
+                        
+                        <td>
+                            <v-chip :color="item.status === 'O' ? 'info' : 'error'">
+                                {{ item.status }}
+                            </v-chip>
+                               </td>
                     </tr>
                 </tbody>
             </v-table>
