24099b5e10cd62308549a982126981194d290290
lcw3176 <lcw3176@naver.com>
2023-07-06 12:04:43 +0900

    feat: 광고 스크립트 추가

--- src/views/AdsView.vue
+++ src/views/AdsView.vue
@@ -2,19 +2,19 @@
   <v-container fluid>
     <v-row class="text-center">
 
-      <v-col>
+      <v-col id="adOne">
         <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-OPghfSGHOLdGJdlJ" data-ad-width="300"
           data-ad-height="250"></ins>
-
+        
       </v-col>
 
 
-      <v-col>
+      <v-col id="adTwo">
         <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-1f22sNeH6puxNSbl" data-ad-width="300"
           data-ad-height="250"></ins>
       </v-col>
 
-      <v-col>
+      <v-col id="adThree">
         <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-HEHh99g6pdekjkfQ" data-ad-width="300"
           data-ad-height="250"></ins>
       </v-col>
@@ -29,14 +29,30 @@
 export default {
   name: 'AdsView',
 
-  setup() {
-    const plugin = document.createElement("script");
-    plugin.setAttribute(
+  mounted() {
+    let adOne = document.createElement("script");
+    adOne.setAttribute(
       "src",
       "//t1.daumcdn.net/kas/static/ba.min.js"
     );
-    plugin.async = true;
-    document.head.appendChild(plugin);
+    adOne.async = true;
+    document.getElementById("adOne").appendChild(adOne);
+
+    let adTwo = document.createElement("script");
+    adTwo.setAttribute(
+      "src",
+      "//t1.daumcdn.net/kas/static/ba.min.js"
+    );
+    adTwo.async = true;
+    document.getElementById("adTwo").appendChild(adTwo);
+
+    let adThree = document.createElement("script");
+    adThree.setAttribute(
+      "src",
+      "//t1.daumcdn.net/kas/static/ba.min.js"
+    );
+    adThree.async = true;
+    document.getElementById("adThree").appendChild(adThree);
   }
 
 }
