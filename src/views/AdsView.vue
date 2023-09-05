89997edacf5f282173577576feb9869b425b283a
lcw3176 <lcw3176@naver.com>
2023-09-05 13:38:55 +0900

    광고 쿠팡 파트너스로 교체

--- src/views/AdsView.vue
+++ src/views/AdsView.vue
@@ -2,7 +2,31 @@
   <v-container fluid>
     <v-row class="text-center">
 
-      <v-col id="adOne">
+      <v-col>
+
+      </v-col>
+
+      <v-col cols="6">
+        <v-card class="mx-auto" title="후원하기" subtitle="아래 링크로 상품 검색 후 구매 시, 일정액의 수수료가 개발자에게 제공됩니다">
+          <v-card-item>
+            <div>
+              <iframe src="https://coupa.ng/cebapa" width="100%" height="36" frameborder="0" scrolling="no"
+                referrerpolicy="unsafe-url"></iframe>
+              <div class="text-caption"> 이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.</div>
+            </div>
+          </v-card-item>
+
+        </v-card>
+
+
+      </v-col>
+
+      <v-col>
+
+      </v-col>
+
+
+      <!-- <v-col id="adOne">
         <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-OPghfSGHOLdGJdlJ" data-ad-width="300"
           data-ad-height="250"></ins>
         
@@ -17,7 +41,7 @@
       <v-col id="adThree">
         <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-HEHh99g6pdekjkfQ" data-ad-width="300"
           data-ad-height="250"></ins>
-      </v-col>
+      </v-col> -->
 
     </v-row>
   </v-container>
@@ -29,31 +53,31 @@
 export default {
   name: 'AdsView',
 
-  mounted() {
-    let adOne = document.createElement("script");
-    adOne.setAttribute(
-      "src",
-      "//t1.daumcdn.net/kas/static/ba.min.js"
-    );
-    adOne.async = true;
-    document.getElementById("adOne").appendChild(adOne);
+  // mounted() {
+    // let adOne = document.createElement("script");
+    // adOne.setAttribute(
+    //   "src",
+    //   "//t1.daumcdn.net/kas/static/ba.min.js"
+    // );
+    // adOne.async = true;
+    // document.getElementById("adOne").appendChild(adOne);
 
-    let adTwo = document.createElement("script");
-    adTwo.setAttribute(
-      "src",
-      "//t1.daumcdn.net/kas/static/ba.min.js"
-    );
-    adTwo.async = true;
-    document.getElementById("adTwo").appendChild(adTwo);
+    // let adTwo = document.createElement("script");
+    // adTwo.setAttribute(
+    //   "src",
+    //   "//t1.daumcdn.net/kas/static/ba.min.js"
+    // );
+    // adTwo.async = true;
+    // document.getElementById("adTwo").appendChild(adTwo);
 
-    let adThree = document.createElement("script");
-    adThree.setAttribute(
-      "src",
-      "//t1.daumcdn.net/kas/static/ba.min.js"
-    );
-    adThree.async = true;
-    document.getElementById("adThree").appendChild(adThree);
-  }
+    // let adThree = document.createElement("script");
+    // adThree.setAttribute(
+    //   "src",
+    //   "//t1.daumcdn.net/kas/static/ba.min.js"
+    // );
+    // adThree.async = true;
+    // document.getElementById("adThree").appendChild(adThree);
+  // }
 
 }
 </script>
