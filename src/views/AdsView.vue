a2b13f0e20c9db05069ae79021afa1d7ecac56f8
lcw3176 <lcw3176@naver.com>
2023-09-19 18:46:10 +0900

    광고 변경중

--- src/views/AdsView.vue
+++ src/views/AdsView.vue
@@ -2,38 +2,14 @@
   <v-container fluid>
     <v-row class="text-center">
 
-      <v-col>
-
-      </v-col>
-
-      <v-col cols="6">
-        <v-card class="mx-auto" title="후원하기" subtitle="아래 링크로 상품 검색 후 구매 시, 일정액의 수수료가 개발자에게 제공됩니다">
-          <v-card-item>
-            <div>
-              <iframe src="https://coupa.ng/cebapa" width="100%" height="36" frameborder="0" scrolling="no"
-                referrerpolicy="unsafe-url"></iframe>
-              <div class="text-caption"> 이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.</div>
-            </div>
-          </v-card-item>
-
-        </v-card>
-
-
-      </v-col>
-
-      <v-col>
+      <v-col id="adOne">
+        <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-jQc4S43mgtM6ZqRE" data-ad-width="728"
+          data-ad-height="90"></ins>
 
       </v-col>
 
 
-      <!-- <v-col id="adOne">
-        <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-OPghfSGHOLdGJdlJ" data-ad-width="300"
-          data-ad-height="250"></ins>
-        
-      </v-col>
-
-
-      <v-col id="adTwo">
+      <!-- <v-col id="adTwo">
         <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-1f22sNeH6puxNSbl" data-ad-width="300"
           data-ad-height="250"></ins>
       </v-col>
@@ -53,14 +29,15 @@
 export default {
   name: 'AdsView',
 
-  // mounted() {
-    // let adOne = document.createElement("script");
-    // adOne.setAttribute(
-    //   "src",
-    //   "//t1.daumcdn.net/kas/static/ba.min.js"
-    // );
-    // adOne.async = true;
-    // document.getElementById("adOne").appendChild(adOne);
+  mounted() {
+
+    let adOne = document.createElement("script");
+    adOne.setAttribute(
+      "src",
+      "//t1.daumcdn.net/kas/static/ba.min.js"
+    );
+    adOne.async = true;
+    document.getElementById("adOne").appendChild(adOne);
 
     // let adTwo = document.createElement("script");
     // adTwo.setAttribute(
@@ -77,7 +54,7 @@ export default {
     // );
     // adThree.async = true;
     // document.getElementById("adThree").appendChild(adThree);
-  // }
+  }
 
 }
 </script>
