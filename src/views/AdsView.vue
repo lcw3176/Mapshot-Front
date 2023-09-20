f837bfb247ad2969e7dc17f19d396660ef8c7ac4
lcw3176 <lcw3176@naver.com>
2023-09-20 20:09:14 +0900

    광고 정리

--- src/views/AdsView.vue
+++ src/views/AdsView.vue
@@ -4,22 +4,32 @@
 
       <v-col>
         <div id="adOne">
-          <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-jQc4S43mgtM6ZqRE" data-ad-width="728"
-            data-ad-height="90"></ins>
-
-        </div>
-
-        <div id="adTwo">
           <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-5wPT27aGQHQre4DP" data-ad-width="320"
             data-ad-height="100"></ins>
         </div>
 
       </v-col>
 
-      <v-col id="adThree">
-        <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-HEHh99g6pdekjkfQ" data-ad-width="300"
-          data-ad-height="250"></ins>
+
+      <v-col>
+
+        <div id="adTwo">
+          <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-PHtjbTNT6UriScBj" data-ad-width="320"
+            data-ad-height="50"></ins>
+        </div>
       </v-col>
+
+
+      <v-col>
+        <div id="adThree">
+          <ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-jQc4S43mgtM6ZqRE" data-ad-width="728"
+            data-ad-height="90"></ins>
+
+        </div>
+
+      </v-col>
+
+
 
     </v-row>
 
@@ -67,30 +77,33 @@ export default {
   },
 
   mounted() {
-    
-    let adOne = document.createElement("script");
-    adOne.setAttribute(
-      "src",
-      "//t1.daumcdn.net/kas/static/ba.min.js"
-    );
-    adOne.async = true;
-    document.getElementById("adOne").appendChild(adOne);
 
-    let adTwo = document.createElement("script");
-    adTwo.setAttribute(
-      "src",
-      "//t1.daumcdn.net/kas/static/ba.min.js"
-    );
-    adTwo.async = true;
-    document.getElementById("adTwo").appendChild(adTwo);
+    if (this.display.mdAndUp) {
+      let adOne = document.createElement("script");
+      adOne.setAttribute(
+        "src",
+        "//t1.daumcdn.net/kas/static/ba.min.js"
+      );
+      adOne.async = true;
+      document.getElementById("adOne").appendChild(adOne);
 
-    let adThree = document.createElement("script");
-    adThree.setAttribute(
-      "src",
-      "//t1.daumcdn.net/kas/static/ba.min.js"
-    );
-    adThree.async = true;
-    document.getElementById("adThree").appendChild(adThree);
+      let adTwo = document.createElement("script");
+      adTwo.setAttribute(
+        "src",
+        "//t1.daumcdn.net/kas/static/ba.min.js"
+      );
+      adTwo.async = true;
+      document.getElementById("adTwo").appendChild(adTwo);
+
+      let adThree = document.createElement("script");
+      adThree.setAttribute(
+        "src",
+        "//t1.daumcdn.net/kas/static/ba.min.js"
+      );
+      adThree.async = true;
+      document.getElementById("adThree").appendChild(adThree);
+    }
+
   }
 
 }
