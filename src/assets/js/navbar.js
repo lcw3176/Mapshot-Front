c21640b66c0a69a64223cb9cc79473d7e0bb05d6
lcw3176 <lcw3176@naver.com>
2023-12-06 22:43:50 +0900

    불필요한 navbar.js 삭제

--- src/assets/js/navbar.js
+++
@@ -1,9 +0,0 @@
-navbarBurgerClick = function navbarBurgerClick(id) {
-    if (id.getAttribute("class") === "navbar-burger") {
-        id.setAttribute("class", "navbar-burger is-active");
-        document.getElementById("navbar-menu").setAttribute("class", "navbar-menu is-active");
-    } else {
-        id.setAttribute("class", "navbar-burger");
-        document.getElementById("navbar-menu").setAttribute("class", "navbar-menu");
-    }
-};
