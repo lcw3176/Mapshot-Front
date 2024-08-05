114950923ea8dabdb278ae1b8201675a0c99c5a7
lcw3176 <lcw3176@naver.com>
2024-08-05 16:20:36 +0900

    태그 위치 변경

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -143,10 +143,6 @@
           도시 계획 레이어
         </v-list-item>
 
-        <v-switch density="compact" v-if="mapStore.company === 'google'" v-model="mapStore.noLabel"  color="info">
-          지형지물 명칭 없애기
-        </v-switch>
-
         <v-overlay :model-value="overlay" class="align-center justify-center">
           <v-card>
             <v-container>
@@ -177,6 +173,9 @@
 
           <v-switch density="compact" color="info" v-if="mapStore.company === 'naver'" v-model="mapStore.onlyLayers"
             label="레이어만 출력하기" />
+
+          <v-switch density="compact" color="info" v-if="mapStore.company === 'google'" v-model="mapStore.noLabel"
+            label=" 지형지물 명칭 없애기"/>
 
           <v-radio-group v-if="mapStore.onlyLayers" inline v-model="mapStore.layerExtension">
             <v-radio label="PNG" value="image/png"></v-radio>
