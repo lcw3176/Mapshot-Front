07ae94768186bc6237778b6adaac4dfc6b711b70
lcw3176 <lcw3176@naver.com>
2024-10-01 21:40:22 +0900

    메서드 교체

--- src/views/NoticeSummaryView.vue
+++ src/views/NoticeSummaryView.vue
@@ -1,136 +1,138 @@
 <template>
-    <v-container is-fluid>
+  <v-container is-fluid>
 
-      <v-row v-if="display.mdAndUp">
-        <v-col>
+    <v-row v-if="display.mdAndUp">
+      <v-col>
 
-        </v-col>
+      </v-col>
 
-        <v-col cols="8"  v-if="this.noticeStore.loading" >
-          <v-skeleton-loader type="table">
-
-
-          </v-skeleton-loader>
-
-        </v-col>
+      <v-col cols="8" v-if="this.noticeStore.loading">
+        <v-skeleton-loader type="table">
 
 
-        <v-col v-else cols="8">
-          <v-table class="text-body-1">
-            <thead>
-            <tr>
-              <th class="text-left">
-                No
-              </th>
-              <th class="text-left">
-                카테고리
-              </th>
+        </v-skeleton-loader>
 
-              <th class="text-left">
-                제목
-              </th>
-
-              <th class="text-left">
-                작성일
-              </th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="notice in noticeStore.notices" :key="notice.id">
-
-              <td>{{ notice.id }}</td>
-              <td>
-                <v-chip :color=noticeStore.getNoticeTypeClass(notice.noticeType) variant="outlined">
-                  {{ notice.noticeType }}
-                </v-chip>
-              </td>
-              <td><v-list-item :to="{ path: `/notice/detail/${notice.id}` }"> {{ notice.title
-                }}</v-list-item>
-              </td>
-              <td>{{ noticeStore.formatDate(notice.createdDate) }}</td>
-            </tr>
-            </tbody>
-          </v-table>
-
-          <v-pagination v-model="noticeStore.nowPage" @click="noticeStore.loadPostList(noticeStore.nowPage)"  :length="noticeStore.totalPage"></v-pagination>
-
-        </v-col>
-
-        <v-col>
-
-        </v-col>
-      </v-row>
+      </v-col>
 
 
+      <v-col v-else cols="8">
+        <v-table class="text-body-1">
+          <thead>
+          <tr>
+            <th class="text-left">
+              No
+            </th>
+            <th class="text-left">
+              카테고리
+            </th>
+
+            <th class="text-left">
+              제목
+            </th>
+
+            <th class="text-left">
+              작성일
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="notice in noticeStore.notices" :key="notice.id">
+
+            <td>{{ notice.id }}</td>
+            <td>
+              <v-chip :color=noticeStore.getNoticeTypeClass(notice.noticeType) variant="outlined">
+                {{ notice.noticeType }}
+              </v-chip>
+            </td>
+            <td>
+              <v-list-item :to="{ path: `/notice/detail/${notice.id}` }"> {{
+                  notice.title
+                }}
+              </v-list-item>
+            </td>
+            <td>{{ noticeStore.formatDateWithoutHour(notice.createdDate) }}</td>
+          </tr>
+          </tbody>
+        </v-table>
+
+        <v-pagination v-model="noticeStore.nowPage" @click="noticeStore.loadPostList(noticeStore.nowPage)"
+                      :length="noticeStore.totalPage"></v-pagination>
+
+      </v-col>
+
+      <v-col>
+
+      </v-col>
+    </v-row>
 
 
-      <v-row v-else>
+    <v-row v-else>
 
-        <v-col v-if="this.noticeStore.loading" >
-          <v-skeleton-loader type="table">
+      <v-col v-if="this.noticeStore.loading">
+        <v-skeleton-loader type="table">
 
-          </v-skeleton-loader>
-        </v-col>
+        </v-skeleton-loader>
+      </v-col>
 
-        <v-col v-else>
+      <v-col v-else>
 
-          <v-table>
-            <thead>
-            <tr>
-              <th class="text-left">
-                제목
-              </th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="notice in noticeStore.notices" :key="notice.id">
+        <v-table>
+          <thead>
+          <tr>
+            <th class="text-left">
+              제목
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="notice in noticeStore.notices" :key="notice.id">
 
-              <td>
-                <v-list-item :to="{ path: `/notice/detail/${notice.id}` }">
-                  {{ notice.title }}
-                </v-list-item>
-              </td>
-            </tr>
-            </tbody>
-          </v-table>
+            <td>
+              <v-list-item :to="{ path: `/notice/detail/${notice.id}` }">
+                {{ notice.title }}
+              </v-list-item>
+            </td>
+          </tr>
+          </tbody>
+        </v-table>
 
-          <v-pagination v-model="noticeStore.nowPage" @click="noticeStore.loadPostList(noticeStore.nowPage)"  :length="noticeStore.totalPage"></v-pagination>
+        <v-pagination v-model="noticeStore.nowPage" @click="noticeStore.loadPostList(noticeStore.nowPage)"
+                      :length="noticeStore.totalPage"></v-pagination>
 
-        </v-col>
-      </v-row>
-    </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 
 </template>
 
 <script>
-import { useNoticeStore } from '@/store/notice';
+import { useNoticeStore } from '@/store/notice'
 import { ref } from 'vue'
 import { useDisplay } from 'vuetify'
 
 export default {
-    name: 'NoticeView',
-    data() {
-        const display = ref(useDisplay());
+  name: 'NoticeView',
+  data () {
+    const display = ref(useDisplay())
 
+    return {
+      notices: [],
+      loadNum: 0,
+      display
 
-        return {
-            notices: [],
-            loadNum: 0,
-            display
+    }
+  },
 
-        }
-    },
+  setup () {
+    const noticeStore = useNoticeStore()
 
-    setup() {
-        const noticeStore = useNoticeStore();
-
-        return {
-            noticeStore
-        }
-    },
+    return {
+      noticeStore
+    }
+  },
 
   created () {
-    this.noticeStore.loadPostList(this.noticeStore.nowPage);
+    this.noticeStore.loadPostList(this.noticeStore.nowPage)
 
   }
 }
