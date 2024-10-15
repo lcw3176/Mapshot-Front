966d80f98fa31cee952f19b6812202231ee1a2f4
Chanwoo Lee <chanwoo3176@naver.com>
2024-10-16 08:22:57 +0900

    내용 업데이트

--- src/components/faq/HowToCapture.vue
+++ src/components/faq/HowToCapture.vue
@@ -2,31 +2,43 @@
   <v-card elevation="0">
     <v-card-title class="text-h5">캡쳐는 어떻게 하나요?</v-card-title>
 
-    <!--    <v-card elevation="0">-->
-    <!--      <v-img :src="greytile" max-height="500"></v-img>-->
-    <!--    </v-card>-->
-
     <v-card-text>
-      <!--      종종 지도 결과물에 회색 타일이 섞인 채 생성되는 경우가 존재합니다.-->
+      '크롬 웹 스토어'에 접속하시면 여러가지 캡쳐 관련 확장 프로그램들이 존재하는데요, 
+      반드시 크롬 브라우저가 아니어도 익스플로러를 제외한 대부분의 브라우저에서 사용 가능합니다.
     </v-card-text>
 
     <v-card-text>
-      <!--      해당 현상 발생 시 Ctrl + 휠을 통해 브라우저의 확대 수준을 조절하거나, 잠시만 기다려 주시면 로딩이 완료됩니다.-->
+      대표적으로 2가지 프로그램을 추천드리는데요,
+      full page screen capture 라는 프로그램과 fireshot이라는 프로그램을 추천드립니다. 링크는 다음과 같습니다.
+
+      full page screen capture
+      - https://chromewebstore.google.com/detail/gofullpage-full-page-scre/fdpohaocaechififmbbbbbknoalclacl
+
+      fireshot
+      - https://chromewebstore.google.com/detail/take-webpage-screenshots/mcbpblocgmgfnpjjppndjkmgjaogfceg
     </v-card-text>
+
+      <v-card-text>
+        해당 프로그램들을 이용하시면 jpg, png 등의 확장자로 웹 페이지를 고화질로 캡쳐하실 수 있습니다. 
+      템플릿 제작 버튼 누르시고 지도 타일 로딩이 전부 완료된 후 ,위의 프로그램들을 이용해서 페이지 전체 캡쳐를 진행하시면 예전처럼 큰 사진을 만드실 수 있을 거에요.
+      화질은 첫 번째로 추천 드린 프로그램이 조금 더 우수합니다. 그리고 참조할만한 레퍼런스도 첫 번째 프로그램이 훨씬 많아서, 되도록이면 첫 번째 프로그램 사용을 추천드려요.
+    </v-card-text>
+
+      
+
 
 
   </v-card>
 </template>
 
 <script>
-// import greytile from '@/assets/img/faq/greytile.jpg'
 
 export default {
   name: 'HowToCapture',
 
   data () {
     return {
-      // greytile: greytile
+      
     }
   }
 }
