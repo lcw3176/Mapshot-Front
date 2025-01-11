1c3d2745b7247666f97bd4e06e14b2771be11bb2
Chanwoo Lee <chanwoo3176@naver.com>
2025-01-12 00:03:31 +0900

    옵션 제거

--- src/main.js
+++ src/main.js
@@ -19,8 +19,7 @@ Sentry.init({
     dsn: "https://1c1f8712a989433cb34416dd0bfde82e@o4505030048284672.ingest.sentry.io/4505030053330944",
     integrations: [
         new Sentry.BrowserTracing({
-            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-            tracePropagationTargets: ["kmapshot.com", /^\//],
+            routingInstrumentation: Sentry.vueRouterInstrumentation(router)
         }),
     ],
 
