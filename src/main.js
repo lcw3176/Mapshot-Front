7fa44c0e7b700fbd557160f6fdb53b452c941426
lcw3176 <lcw3176@naver.com>
2025-01-12 12:56:45 +0900

    센트리 추적 옵션 추가

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createPinia } from "pinia"
+import { createPinia } from 'pinia'
 import VueMobileDetection from 'vue-mobile-detection'
-import * as Sentry from "@sentry/vue";
+import * as Sentry from '@sentry/vue'
 
 // Vuetify
 import '@mdi/font/css/materialdesignicons.css'
@@ -12,24 +12,27 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-
-
 Sentry.init({
-    App,
-    dsn: "https://1c1f8712a989433cb34416dd0bfde82e@o4505030048284672.ingest.sentry.io/4505030053330944",
-    integrations: [
-        new Sentry.BrowserTracing({
-            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-            tracePropagationTargets: ["kmapshot.com", /^\//],
-        }),
-    ],
+  App,
+  dsn: 'https://1c1f8712a989433cb34416dd0bfde82e@o4505030048284672.ingest.sentry.io/4505030053330944',
+  integrations: [
+    Sentry.browserTracingIntegration({ router }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
 
-    tracesSampleRate: 1,
-});
+  tracePropagationTargets: ['kmapshot.com', /^\//],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+
+})
 
 const vuetify = createVuetify({
-    components,
-    directives,
+  components,
+  directives,
 })
 
 createApp(App).use(createPinia()).use(router).use(vuetify).use(VueMobileDetection).mount('#app')
