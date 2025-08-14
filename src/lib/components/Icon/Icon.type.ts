a2b9c5e407f6662915d9d3fbe4b0380325129da0
Lee Jae Yeong <leey153@naver.com>
2025-08-14 11:19:04 +0900

    ✏️ Feat : Icon Global Type & Close Icon

---
+++ src/lib/components/Icon/Icon.type.ts
@@ -0,0 +1,6 @@
+import type { SVGProps } from "react";
+
+export interface IconCommonProps extends SVGProps<SVGSVGElement> {
+  size?: string;
+  fill?: string;
+}
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Button } from "./Button/Button";
+export { default as Modal } from "./Modal/Modal";
+
+export * from "./Icon";
